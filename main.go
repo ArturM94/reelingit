b7d5dcdc30af741d244a9ec806acd49f80ab8458
Artur Manukian <manukian.artur.94@gmail.com>
2025-06-07 18:47:15 +0300

    feat: add logger

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/ArturM94/reelingit/logger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+
+	if err != nil {
+		log.Fatalf("Failed to initialize logger %v", err)
+		return nil
+	}
+
+	defer logInstance.Close()
+
+	return logInstance
+}
+
 func main() {
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
 	}
 }
