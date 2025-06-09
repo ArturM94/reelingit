20f7bb4a8023a406cf918f3d29be3223e314549b
Artur Manukian <manukian.artur.94@gmail.com>
2025-06-09 17:16:50 +0300

    feat: add basic html structure and api.js module

---
+++ public/app.js
@@ -0,0 +1,10 @@
+window.app = {
+  /**
+   * @param {Event} event
+   */
+  search: (event) => {
+    event.preventDefault();
+    const q = document.querySelector('input[type=search]').value;
+    // TODO
+  },
+};
