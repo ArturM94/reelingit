e7b0d27f040a3b5eb97cf14c1b604c0836a9f380
Artur Manukian <manukian.artur.94@gmail.com>
2025-07-14 13:28:09 +0300

    feat: create JWT on the backend

---
+++ token/getsecret.go
@@ -0,0 +1,18 @@
+package token
+
+import (
+	"github.com/ArturM94/reelingit/logger"
+	"os"
+)
+
+func GetJWTSecret(logger logger.Logger) string {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		jwtSecret = "default-secret-for-dev"
+		logger.Info("JWT_SECRET not set, using default development secret")
+	} else {
+		logger.Info("Using JWT_SECRET from environment")
+	}
+
+	return jwtSecret
+}
