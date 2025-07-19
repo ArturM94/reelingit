512cf6be0e33059d19cf08bfbef349a863b5da4c
Artur Manukian <manukian.artur.94@gmail.com>
2025-07-19 20:52:50 +0300

    feat: add AuthMiddleware

--- main.go
+++ main.go
@@ -66,6 +66,9 @@ func main() {
 	http.HandleFunc("/api/account/register", accountHandler.Register)
 	http.HandleFunc("/api/account/authenticate", accountHandler.Authenticate)
 
+	http.Handle("/api/account/favorites", accountHandler.AuthMiddleware(http.HandlerFunc(accountHandler.GetFavorites)))
+	http.Handle("/api/account/watchlist", accountHandler.AuthMiddleware(http.HandlerFunc(accountHandler.GetWatchlist)))
+
 	catchAllClientRoutesHandler := func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./public/index.html")
 	}
